5e0ea6527bc3573779f4f53dda0ac8ff00e069ad
HannahSoderbrg <Hannahsoderb@gmail.com>
2025-04-27 02:05:04 +0200

    Resolve merge conflicts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -14,21 +14,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-=======
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Welcome to Ivy Recruitment Platform API!');
-});
-
-const PORT = process.env.PORT || 5000;
->>>>>>> b6aa89fe9e46258355d631cebe7f07a22e77a7ab
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
